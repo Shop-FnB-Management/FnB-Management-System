d3c9175e4f3bac3defb207ebea4be5f695b32cf0
Rumput <much.adib123@gmail.com>
2026-07-29 12:02:08 +0700

    completing backend project structure

---
+++ backend/db/keys.ts
@@ -0,0 +1,11 @@
+export const Keys = {
+  menu: (id: string) => ["menu", id],
+
+  order: (id: string) => ["orders", id],
+
+  payment: (id: string) => ["payments", id],
+
+  reservation: (id: string) => ["reservations", id],
+
+  admin: (username: string) => ["admins", username],
+};
