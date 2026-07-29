519e2917368e51f351962cb9e9ac4f8155b95b9c
Rumput <much.adib123@gmail.com>
2026-07-29 13:12:19 +0700

    move main.ts to src

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,9 @@ import { Hono } from 'hono'
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  return c.json({
+    message: 'Restaurant Management System API is running successfully!',
+  })
 })
 
 Deno.serve(app.fetch)
